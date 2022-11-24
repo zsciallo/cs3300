dcfb6bdf1339c985fa379acb571e4bdbc35fb2ab
zsciallo <zsciallo@uccs.edu>
2022-11-24 00:08:46 +0000

    Simple devise auth added

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.0.2"
-
+gem 'devise'
 #simplecov
 gem 'simplecov', require: false, group: :test
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
-Rails.application.routes.draw do
-  resources :projects
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  Rails.application.routes.draw do
+ Rails.application.routes.draw do
+  devise_for :users 
     root "projects#index"
     resources :projects
+    devise_scope :user do
+      get '/users/sign_out' => 'devise/sessions#destroy'
+      end
   end
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
